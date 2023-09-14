2925bad6412c79e385666eed527f282f2dfc015d
Declan <144496393+dec-gr@users.noreply.github.com>
2023-09-14 18:28:05 +0100

    Created random color setter

--- javascript.js
+++ javascript.js
@@ -1,23 +1,48 @@
 const container = document.querySelector('.container');
 
-for (j = 0; j < 16; j++){
 
-const row = document.createElement('div');
-row.classList.add('row');                                      
-for (i = 0; i < 16; i++){
-    const div = document.createElement('div');
-    div.classList.add('pixel');                                      
-    row.appendChild(div);
+function createScreen (numberOfPixels = 16) {
+
+    for (j = 0; j < numberOfPixels; j++){
+
+    const row = document.createElement('div');
+    row.classList.add('row');                                      
+    for (i = 0; i < numberOfPixels; i++){
+        const div = document.createElement('div');
+        div.classList.add('pixel');                           
+        row.appendChild(div);
+    }
+
+    container.appendChild(row);
+
+    }
+
+    const pixels = document.querySelectorAll(`.pixel`);
+
+    pixels.forEach((pixel) => {
+    
+        //pixel.addEventListener("mouseover", () => pixel.classList.add('filled'))
+        pixel.addEventListener("mouseover", setPixelRandomColor)
+
+    } )
 }
 
-container.appendChild(row);
+function setPixelRandomColor(e){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.background = `#${randomColor}`;
+}
+
+
+
+function clearScreen() {
+    container.replaceChildren()
+    let numberOfPixels = Number(window.prompt("How many pixels?", ""));
+    numberOfPixels = (numberOfPixels<=100 && numberOfPixels>=16) ? numberOfPixels : 16
+    createScreen(numberOfPixels)
 
 }
 
-const pixels = document.querySelectorAll(`.pixel`);
+const clearBtn = document.querySelector("#clearBtn")
+clearBtn.addEventListener("click", clearScreen)
 
-pixels.forEach((pixel) => {
-  
-    pixel.addEventListener("mouseover", () => pixel.classList.toggle('filled'))
-  
-} )
+createScreen()
